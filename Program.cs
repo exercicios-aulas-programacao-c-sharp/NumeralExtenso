28bc926c9d82212aab6c34464f9b13d5d8c681da
diego_neri <diego@diegoneri.com>
2020-06-05 20:08:47 -0300

    Finaliza o exercício NumeralExtenso

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Humanizer;
+
+namespace NumeralExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.Write("Digite um número ..: ");
+            
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"\n{numero.ToWords()}");
+
+        }
+    }
+}
